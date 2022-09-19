a8d8a744229a552b84d558578f72b5dae190af67
jakerules <jakobgrossman242@gmail.com>
2022-09-19 17:49:02 -0400

    ahhhhhh

--- ex5.py
+++ ex5.py
@@ -6,5 +6,6 @@ t=int(input("Enter year ammount: "))
 
 #Function Definitions
 def interest(p,r,t,n):
-    print(p((1+(r/n))(n*t)))
-print("Your new balence is: " , interest)
+    return (p((1+(r/n))(n*t)))
+#Print
+print("Your new balence is: " , interest(p,r,t,n)
