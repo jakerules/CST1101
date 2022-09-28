14d9b51904e341d99fb1f13756490230007c03d2
jakerules <jakobgrossman242@gmail.com>
2022-09-28 16:50:48 -0400

    450

--- 9-28-22.py
+++ 9-28-22.py
@@ -3,6 +3,25 @@
 #    
 #}
 
-x = 2 #Assignment Statement!!!!!!
+#x = 5 #Assignment Statement!!!!!!
+#if(x>3):
+#    print("Hello!")
+
+# x = 2 #Assignment Statement!!!!!!
+# if(x>3):
+#    print("Hello!")
+
+'''
+x=3
 if(x>3):
-    print("Hello!")
+    print("Hello ")
+    print("world!")
+    print(5*2/3)
+'''
+'''
+<
+>
+==
+<=
+>=
+'''
