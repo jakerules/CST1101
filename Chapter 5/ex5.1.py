646b65380cc3586d23657495bb7b53d08ed7cc51
jakerules <jakerules@users.noreply.github.com>
2022-10-12 13:50:28 -0400

    150

--- Chapter 5/ex5.1.py
+++ Chapter 5/ex5.1.py
@@ -1,20 +1,22 @@
 #Define and set variable
-day = int(input("Input value: "))
+def get_day_name(day):
 
 #Find and print day
-if day == 0:
-    print("Sunday")
-elif day == 1:
-    print("Monday")
-elif day == 2:
-    print("Tuesday")
-elif day == 3:
-    print("Wednesday")
-elif day == 4:
-    print("Thursday")
-elif day == 5:
-    print("Friday")
-elif day == 6:
-    print("Saturday")
-else:
-    print("Not a valid number!")
+    if day == 0:
+        print("Sunday")
+    elif day == 1:
+        print("Monday")
+    elif day == 2:
+        print("Tuesday")
+    elif day == 3:
+        print("Wednesday")
+    elif day == 4:
+        print("Thursday")
+    elif day == 5:
+        print("Friday")
+    elif day == 6:
+        print("Saturday")
+    else:
+        print("Not a valid number!")
+#Run Function
+get_day_name(day = int(input("Input value: ")))
---
+++ Chapter 5/ex5.2.py
@@ -0,0 +1,4 @@
+from ex5.1 import get_day_name
+#Define
+start = int(input("What is the starting day of the week number?"))
+length = int(input("What is the number of days that passed?"))
