e24e75f6fa6702ab43555b8bc794b00ace5aa6fc
jakerules <jakerules@users.noreply.github.com>
2022-10-12 13:03:57 -0400

    103

--- Chapter 5/ex5.1.py
+++ Chapter 5/ex5.1.py
@@ -10,3 +10,11 @@ elif day == 2:
     print("Tuesday")
 elif day == 3:
     print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+else:
+    print("Not a valid number!")
