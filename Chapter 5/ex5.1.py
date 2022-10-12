d18e8a7e14dd578b889654c1148fcc514117b4a1
jakerules <jakerules@users.noreply.github.com>
2022-10-12 13:01:25 -0400

    101

---
+++ Chapter 5/ex5.1.py
@@ -0,0 +1,12 @@
+#Define and set variable
+day=int(input("Input value: "))
+
+#Find and print day
+if day == 0:
+    print("Sunday")
+elif day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
