83660dadafdd9c0ad191575b9465e40385533a6f
jakerules <jakerules@users.noreply.github.com>
2022-10-17 17:00:35 -0400

    yep

--- Test 1/Question 2.py
+++ Test 1/Question 2.py
@@ -4,33 +4,15 @@
 x = int(input("Enter number 1"))
 y = int(input("Enter number 2"))
 
-a = -1
-b = -1
 #num1 calculation
-def part1(x, y, a, b):
+def part1():
     if x > y:
-        maxix = x
-        print("num1 is max")
-        return maxix
-        a = 1
-    elif x < y:
-        print("num1 is min")
-        a = 0
+        max = x
+        min = y
     else:
-        print("Invalid")
+        max = y
+        min = x
 
-    if y > x:
-        maxiy = y
-        b =1
-        print("num2 is max")
-        return maxiy
-    elif y < x:
-        miniy = y
-        print("num2 is min")
-        b = 0
-    else:
-        print("Invalid")
-part1(x, y, a, b)
-print(a, b)
+print(part1(x, y))
 
 
