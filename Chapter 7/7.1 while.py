216d7aa8fa59ad6d07e04542394081ba17166744
jakerules <jakobgrossman242@gmail.com>
2022-10-31 17:43:01 -0400

    542

---
+++ Chapter 7/7.1 while.py
@@ -0,0 +1,15 @@
+def even_sum_while(n):
+    #print(n)
+    total= 0
+    y = 0
+    
+    while y < len(n):
+        if n[y] % 2 == 0:
+            #print("I am at" , y , n[y])
+            total = total + n[y]
+        print ("I am at list index" ,y," whihc has memebr" ,n[y] ,"curent total = ",total)
+        y = y + 1
+        print("############################################")
+    return total
+
+print(even_sum_while([3,5,6,7,10]))
