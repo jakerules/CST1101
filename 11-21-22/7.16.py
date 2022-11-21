43d66c59c1a294aff33cd894d411e373a8d34eb5
jakerules <jakerules@users.noreply.github.com>
2022-11-21 17:42:03 -0500

    542

--- 11-21-22/7.16.py
+++ 11-21-22/7.16.py
@@ -1,13 +1,19 @@
 ##SQUARE AND ADD ALL IN A LIST
 
 
-#Make List
+#Make List and declare 
 xs = [4, 6, 9, 2]
-
+count = 0
+total = 0
+sum = 0
 #Create Function(s)
 
+#Count list items
+for i in xs:
+    count = count+1
 
 #Do Maths
-
-
+for i in xs:
+    x= i * i
+    total = sum + x
 #Print Result
