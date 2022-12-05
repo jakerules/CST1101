359f0ddfc4f385a5dc004dbb45cffb19ed932a2d
jakerules <jakerules@users.noreply.github.com>
2022-12-05 16:24:06 -0500

    424

---
+++ Test 3/test3.py
@@ -0,0 +1,18 @@
+#Write a function that will remove  Vowels  letters from any text . 
+#Vowels are  "!\""aeiouAEIOU". Make sure that your function is 
+#tested and work with different text examples.
+
+
+
+def rem_vowl():
+    #Get inital input
+    original = input("Original text: ")
+    #Define variables
+    vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'I', 'E', 'O', 'U', '!', '/')
+    new = ""
+    #Start removal
+    for i in original:
+        if i not in vowels:
+            new = new + i
+    #Return text without vowels
+    return new
