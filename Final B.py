2055741d047324ff79efed983e4a7efe0d3ff010
jakerules <jakerules@users.noreply.github.com>
2022-12-19 16:29:48 -0500

    429

--- Final B.py
+++ Final B.py
@@ -1,8 +1,13 @@
+str = input("What is the String?")
+substr = input("What is the substring to count?")
+
+#Define Function
 def count_substr(str, substr):
     count = 0
     for i in range(len(str) - len(substr) + 1):
-        if str[i] == substr [i:i+len(substr)] == substr:
+        if str[i:i+len(substr)] == substr:
             count += 1
     return count
 
-print(count_substr("Mississippi" , "is"))
+count_substr(str, substr)
+
